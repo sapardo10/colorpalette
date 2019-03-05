be5742dbf6db9d837bf870fe128d4818cd762638
Sergio Pardo <spardo@clickdelivery.com>
2019-03-05 16:33:03 -0500

    added some code to change elements in html

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 function randomPalette(){
-
+	var base = 0
+	var top = 71
+	var color = Math.floor((Math.random() * top) + base);
+	var list = new Array()
+	list.add(color)
+	for (var i = 0 ; i < 5; i--) {
+		color = color+top
+		list.add(color)
+	}
+	
+	paintElement('color1', "blue")
+	paintElement('color2', "blue")
+	paintElement('color3', "blue")
+	paintElement('color4', "blue")
+	paintElement('color5', "blue")
 }
-
-
 
 function generateRules(){
 
 }
+
+function paintElement(elementId, color){
+	console.log(elementId)
+	var element = document.getElementById(elementId)
+	element.style.background = color
+}
+
+function cleanState(){
+	for (var i = 1 ; i <= 5; i++) {
+		paintElement('color'+i, "white")
+	}
+}
+
+randomPalette()
