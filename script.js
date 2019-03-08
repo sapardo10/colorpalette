8b14717263564300d87605bf9095a41adc8fd6e1
Sergio Pardo <sa.pardo10@uniandes.edu.co>
2019-03-07 21:50:21 -0500

    completed web page functionality

--- script.js
+++ script.js
@@ -3,25 +3,19 @@ function randomPalette(){
 	var top = 71
 	var color = Math.floor((Math.random() * top) + base);
 	var list = new Array()
-	list.add(color)
-	for (var i = 0 ; i < 5; i--) {
-		color = color+top
-		list.add(color)
+	list.push(hsvToRgb(color/359,1,1))
+	for (var i = 0 ; i < 4; i++) {
+		color = color+top	
+		list.push(hsvToRgb(color/359,1,1))
 	}
-	
-	paintElement('color1', "blue")
-	paintElement('color2', "blue")
-	paintElement('color3', "blue")
-	paintElement('color4', "blue")
-	paintElement('color5', "blue")
-}
-
-function generateRules(){
-
+	paintCssRules(list)
+	for (var i = 1; i <= list.length; i++) {
+		var aux = i-1
+		paintElement('color'+i, 'rgb(' + list[aux][0] + ',' + list[aux][1] + ',' + list[aux][2] + ')')
+	}
 }
 
 function paintElement(elementId, color){
-	console.log(elementId)
 	var element = document.getElementById(elementId)
 	element.style.background = color
 }
@@ -30,6 +24,44 @@ function cleanState(){
 	for (var i = 1 ; i <= 5; i++) {
 		paintElement('color'+i, "white")
 	}
+	paintDefaultTextArea()
 }
 
-randomPalette()
+function paintCssRules(list){
+	 var background = ".website-background{ color: #"+fullColorHex(Math.floor(list[0][0]),Math.floor(list[0][1]),Math.floor(list[0][2]))+";}"
+	 var text = ".element-text{ color: #"+fullColorHex(Math.floor(list[1][0]),Math.floor(list[1][1]),Math.floor(list[1][2]))+";}"
+	 var border = ".element-border{ border-color: #"+fullColorHex(Math.floor(list[2][0]),Math.floor(list[2][1]),Math.floor(list[2][2]))+";}"
+	 var elementBackground = ".element-background{ background-color: #"+fullColorHex(Math.floor(list[3][0]),Math.floor(list[3][1]),Math.floor(list[3][2]))+";}"
+	 var header = ".header{ color: #"+fullColorHex(Math.floor(list[4][0]),Math.floor(list[4][1]),Math.floor(list[4][2]))+";}"
+	 var final = "\n" + background + "\n \n" + text + "\n \n" + border + "\n \n" + elementBackground + "\n \n" + header + "\n \n"
+	 var textArea = document.getElementById('css-rules')
+	 textArea.value = final
+}
+
+function paintDefaultTextArea(){
+	var background = ".website-background{ color: #FFFFFF;}"
+	 var text = ".element-text{ color: #FFFFFF;}"
+	 var border = ".element-border{ border-color: #FFFFFF;}"
+	 var elementBackground = ".element-background{ background-color: #FFFFFF;}"
+	 var header = ".header{ color: #FFFFFF;}"
+	 var final = "\n" + background + "\n \n" + text + "\n \n" + border + "\n \n" + elementBackground + "\n \n" + header + "\n \n"
+	var textArea = document.getElementById('css-rules')
+	 textArea.value = final
+
+}
+
+//Sacado de https://campushippo.com/lessons/how-to-convert-rgb-colors-to-hexadecimal-with-javascript-78219fdb
+function rgbToHex (rgb) { 
+  var hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+}
+
+function fullColorHex(r,g,b) {   
+  var red = rgbToHex(r);
+  var green = rgbToHex(g);
+  var blue = rgbToHex(b);
+  return red+green+blue;
+}
